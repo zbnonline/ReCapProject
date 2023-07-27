6a03a34ad7ea791b73ddfc66401441ef93e69d26
anıl <nbr-nb@hotmail.com>
2023-07-27 15:02:37 +0300

    Startup Cors ayarları yapıldı

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -41,6 +41,8 @@ namespace WebAPI
 
             services.AddControllers();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -78,6 +80,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
